29e736e297c1e9a58517c1922682424f644055d4
FarizqyAkhramN <farizkyakhramnugraha@gmail.com>
2026-04-05 14:33:14 +0700

    second commit

--- TP_MODUL6_103022400132/Program.cs
+++ TP_MODUL6_103022400132/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using TP_MODUL6_103022400132;
+
+namespace TugasMusicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaMusicTrack lagu1 = new SayaMusicTrack("Hati-Hati di Jalan");
+            Console.WriteLine("Kondisi Awal:");
+            lagu1.PrintTrackDetails();
+            lagu1.IncreasePlayCount(150);
+            Console.WriteLine("Setelah Update Play Count:");
+            lagu1.PrintTrackDetails();
+            Console.WriteLine("Tekan sembarang tombol untuk keluar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP_MODUL6_103022400132/SayaMusicTrack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400132
+{
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+            Random rand = new Random();
+            this.id = rand.Next(10000, 100000);
+        }
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("=== Detail Music Track ===");
+            Console.WriteLine($"ID          : {this.id}");
+            Console.WriteLine($"Judul Lagu  : {this.title}");
+            Console.WriteLine($"Play Count  : {this.playCount}");
+            Console.WriteLine("--------------------------\n");
+        }
+    }
+}
